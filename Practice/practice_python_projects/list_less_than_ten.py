8be333c877f1d37f18866a4e4ebd173f3fa997fb
Zach <zach.richter85@gmail.com>
2023-03-21 21:46:42 -0700

    project 3

---
+++ Practice/practice_python_projects/list_less_than_ten.py
@@ -0,0 +1,12 @@
+#write a program that prints out all the elements of the list that are less than 10.
+
+def less_than_ten():
+    user_input = int(input("Enter a number: "))
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    new_list = []
+    for value in a:
+        if value < user_input:
+            new_list.append(value)
+            print(new_list)
+            
+less_than_ten()
---
+++ Practice/practice_python_projects/odd_or_even.py
@@ -0,0 +1,11 @@
+
+
+def odd_or_even():
+    ask_number = int(input("Input a number: "))
+    if ask_number % 2 == 0 :
+        print("This number is even")
+    else:
+        return print("This number is odd")
+    
+odd_or_even()
+    
