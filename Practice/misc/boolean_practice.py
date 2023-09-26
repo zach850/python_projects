5b2e36a1c1f4dc0f5d2fb514b2632125d6c03505
Zach Richter <zach@Zachs-MacBook-Pro.local>
2023-09-25 21:09:15 -0700

    deleted files

--- Practice/misc/boolean_practice.py
+++
@@ -1,1 +0,0 @@
-print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
--- Practice/misc/discount.py
+++
@@ -1,8 +0,0 @@
-def dis(price, discount):
-    disc = 100 - (discount)
-    percent = disc / 100
-    total_discount = price * percent
-    return total_discount
-
-
-print(dis(89, 20))
