db0d79ae166c60515826160c9d3a954ffe7c1e38
Zach <zach.richter85@gmail.com>
2023-10-10 21:41:18 -0700

    completed

--- Practice/misc/coffee_machine.py
+++ Practice/misc/coffee_machine.py
@@ -24,54 +24,67 @@ MENU = {
     }
 }
 
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-# Ask user for input
-coffee_type = input("What would you like? (espresso/latte/cappuccino): ")
 
-def coins(quarters, dimes, nickels, pennies):
-    total = float(quarters * 0.25) + float(dimes * 0.10) + float(nickels * 0.05) + float(pennies * 0.01)
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"​Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
     return total
 
-def track_resources(coffee_type):
-    diff_dict = {}
-    coffee_ingredients = MENU[coffee_type]['ingredients']
 
-    # Calculate the difference between available resources and required ingredients
-    for key in resources:
-        if key in coffee_ingredients:
-            diff_dict[key] = resources[key] - coffee_ingredients[key]
-        else:
-            diff_dict[key] = resources[key]
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
 
-    return diff_dict
-        
 
-track_resources(coffee_type)
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
 
-def coffee(coffee_type):
-    if coffee_type not in MENU:
-        print(f"Error: Coffee type '{coffee_type}' not found in MENU.")
-        return None
 
-    if coffee_type == coffee_type:
-        print("Please insert your coins: " )
-        quarters = float(input("how many quarters?: "))
-        dimes = float(input("how many dimes?: "))
-        nickles = float(input("how many nickles?: "))
-        pennies = float(input("how many pennies?: "))
-        total_money_added = coins(quarters, dimes, nickles, pennies)
-        if total_money_added >= MENU[coffee_type]['cost']:
-            change = round(total_money_added - MENU[coffee_type]['cost'],2)
-            print(f"Here is {change} in change.")
-            print(f"Here is your {coffee_type} ☕️. Enjoy!")
-        else:
-            print("Sorry, not enough money. Money refunded.")
-            #TODO 
-            #make function repeat with while loop 
-        
-        
+is_on = True
+
+while is_on:
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])  
