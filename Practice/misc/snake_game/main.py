4a8c21d9f78a4a4869ed5fff28a5e18b258506be
Zach <zach.richter85@gmail.com>
2023-10-23 22:06:12 -0700

    first commit

---
+++ Practice/misc/snake_game/main.py
@@ -0,0 +1,29 @@
+from turtle import Screen, Turtle 
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+starting_position = [(0,0), (-20,0), (-40,0)]
+
+for position in starting_position:
+    new_segment = Turtle(shape="square")
+    new_segment.color('white')
+    new_segment.goto(position)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
