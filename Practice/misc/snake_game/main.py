a3bc284fa36df0de8b1609055520607e1b2b703e
Zach <zach.richter85@gmail.com>
2023-10-24 21:40:19 -0700

    adding classes

--- Practice/misc/snake_game/main.py
+++ Practice/misc/snake_game/main.py
@@ -1,29 +1,27 @@
-from turtle import Screen, Turtle 
+from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-starting_position = [(0,0), (-20,0), (-40,0)]
+snake = Snake()
 
-for position in starting_position:
-    new_segment = Turtle(shape="square")
-    new_segment.color('white')
-    new_segment.goto(position)
-    
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 
 screen.exitonclick()
