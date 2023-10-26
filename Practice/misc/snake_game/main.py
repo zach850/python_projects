5812743c3dd371dbfa52122838fd1dd4506f3d10
Zach <zach.richter85@gmail.com>
2023-10-25 21:54:15 -0700

    added food.py

--- Practice/misc/snake_game/main.py
+++ Practice/misc/snake_game/main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "w")
@@ -23,5 +25,8 @@ while game_is_on:
 
     snake.move()
 
+    #detect collision with food 
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
