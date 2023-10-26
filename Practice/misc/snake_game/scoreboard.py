36507e51eecc71c00e60f015f52603bf753ce36b
Zach <zach.richter85@gmail.com>
2023-10-25 22:23:24 -0700

    added scoreboard.py

---
+++ Practice/misc/snake_game/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.write(
+            f"Score: {self.score}",
+            align="center",
+            font=("Arial", 24, "normal"),
+        )
+        self.hideturtle()
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.write(
+            f"Score: {self.score}",
+            align=ALIGNMENT,
+            font=FONT,
+        )
