9e061b2f45dc12f7b1fb8c5d42a447c5245afda2
Zach <zach.richter85@gmail.com>
2023-11-24 11:00:39 -0800

    exception handling

--- Practice/misc/NATO-alphabet/main.py
+++ Practice/misc/NATO-alphabet/main.py
@@ -1,19 +1,22 @@
 import pandas
 
+is_on = True
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 nato_dict = {row['letter']:row['code'] for (index,row) in df.iterrows()}
 
+def generate_phonetic():
+    user_input = input("Type a word to convert: ")
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Type a word to convert: ")
-
-word_list = [nato_dict[letter.upper()] for letter in user_input]
-print(word_list)
-
+    try: 
+        word_list = [nato_dict[letter.upper()] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        generate_phonetic()
+    else:
+        print(word_list)
+        
+generate_phonetic()
 
 
 
